851fb7913ddc1433fe503620ce27be776d1a2aec
stolynat <natalya.stolyarchuk@gmail.com>
2015-04-21 23:49:52 +0300

    added function active image

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-window.addEventListener('load', positionImages());
+window.addEventListener('load', init);
+
+function init(){
+	width = 210, 
+	height = 140,
+	positionImages();
+	activeImage();
+	document.getElementById("rundomize").addEventListener("click", rundomize);
+}
 
 function positionImages() {
-	var imagesArray = [], x, y, width = 210, height = 140;
-	var container = document.getElementById("container");
-	for (x=0; x<=2; x++) {
-		for (y=0; y<=2; y++){
+	var imagesArray = [], x, y, 
+	rows = 2,
+	columns = 2,
+	container = document.getElementById("container");
+	for (x=0; x<=rows; x++) {
+		for (y=0; y<=columns; y++){
 			var item = {name: x + "-" + y + ".img", 
 						positionTop: x*height, 
 						positionLeft: y*width};
@@ -17,5 +27,32 @@ function positionImages() {
 		}
 	}
 	console.log(imagesArray);
-	/*container.lastChild.src = "images/blank.jpg";*/
+	container.lastChild.src = "images/blank.jpg";
+	container.lastChild.id = "blank";
 }
+
+function activeImage() {
+	var images = document.getElementsByTagName("img"),
+	blankImage = document.getElementById("blank");
+	for (var i=0; i<images.length-1; i++){
+		var blankLeft = parseInt(blankImage.style.left);
+		var blankTop = parseInt(blankImage.style.top);
+		var imgLeft = parseInt(images[i].style.left);
+		var imgTop = parseInt(images[i].style.top);
+		if ((blankTop == imgTop && blankLeft == imgLeft + width) || 
+		    (blankLeft == imgLeft && blankTop == imgTop + height) ||
+			(blankTop == imgTop && blankLeft == imgLeft - width) ||
+			(blankLeft == imgLeft && blankTop == imgTop - height)) {
+			images[i].className = "active";
+		}
+	}
+}
+
+function rundomize() {
+	
+}
+
+
+
+
+
